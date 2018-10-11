a1f6e7a8c06bc2c6acc56f645bea3dcada273987
Quincy Lam <qlam3@my.bcit.ca>
2018-10-10 23:08:49 -0700

    Added functionality to main page

--- app/src/main/java/com/example/quincy/a00994454asn1/SearchActivity.java
+++ app/src/main/java/com/example/quincy/a00994454asn1/SearchActivity.java
@@ -1,13 +1,23 @@
 package com.example.quincy.a00994454asn1;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.quincy.a00994454asn1.helpers.photoManagementHelper;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -16,38 +26,95 @@ public class SearchActivity extends AppCompatActivity {
     /**
      * Dummy data
      */
-    ArrayList<String> dateData = new ArrayList<String>(Arrays.asList("Mt. Fuji", "Mt. Everestt", "Disneyland", "Disneyworld"));
+    //ArrayList<String> dateData = new ArrayList<String>(Arrays.asList("Mt. Fuji", "Mt. Everestt", "Disneyland", "Disneyworld"));
     private photoManagementHelper pmHelper = new photoManagementHelper();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
-        //Initialize Target Button
-        final Button button = findViewById(R.id.searchConfirm);
-
-        //Initialize request text box
-        final TextView requestText = findViewById(R.id.searchBox);
-
-        //Initialize result text box
-        final TextView resultText = findViewById(R.id.resultText);
-
-        //Add listener onto button
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String request = requestText.getText().toString();
-
-                //Get requested text and see if it's in data, return something by result
-                if (pmHelper.find(dateData, request)) {
-                    resultText.setText("Found something");
-                } else {
-                    resultText.setText("None found");
-                }
-            }
-        });
     }
 
+    public void findByCaption(View view) {
+        //Initialize Target Button
+        Button button = findViewById(R.id.searchConfirm);
 
+        //Initialize result text box
+        TextView resultText = findViewById(R.id.resultText);
+
+        //Initialize request text box
+        TextView requestText = findViewById(R.id.searchBox);
+
+        //Initialize image area
+        final ImageView mImageView = findViewById(R.id.photoFound);
+
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        final File[] files = dir.listFiles();
+
+        ArrayList<Pair<String, String>> matchedFiles = new ArrayList<Pair<String, String>>();
+
+        String request = requestText.getText().toString();
+        for (File file : files) {
+            try {
+                String filePath = file.getAbsolutePath();
+                ExifInterface existInterface = new ExifInterface(filePath);
+                String description = existInterface.getAttribute(ExifInterface.TAG_IMAGE_DESCRIPTION);
+
+                if (description.equalsIgnoreCase(request)) {
+                    matchedFiles.add(new Pair<String, String>(filePath, description));
+                }
+            } catch (Exception E) {
+                System.out.println("Could not get metadata");
+                continue;
+            }
+        }
+        //Get requested text and see if it's in data, return something by result
+        if (matchedFiles.size() > 0) {
+            resultText.setText("Found something");
+            //setPic(matchedFiles.get(0).first);
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+//            requestText.clearFocus();
+        } else {
+            resultText.setText("None found");
+        }
+    }
+
+//    private void setPic(String mCurrentPhotoPath) {
+//        ImageView mImageView = findViewById(R.id.photoFound);
+//        // Get the dimensions of the View
+//        int targetW = mImageView.getWidth();
+//        int targetH = mImageView.getHeight();
+//
+//        // Get the dimensions of the bitmap
+//        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+//        bmOptions.inJustDecodeBounds = true;
+//        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+//        int photoW = bmOptions.outWidth;
+//        int photoH = bmOptions.outHeight;
+//
+//        // Determine how much to scale down the image
+//        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+//
+//        // Decode the image file into a Bitmap sized to fill the View
+//        bmOptions.inJustDecodeBounds = false;
+//        bmOptions.inSampleSize = scaleFactor;
+//        bmOptions.inPurgeable = true;
+//
+//        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+//        bitmap = RotateBitmap(bitmap, 90);
+//        mImageView.setImageBitmap(bitmap);
+//    }
+
+    public static Bitmap RotateBitmap(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+
+    public void endSearch(View view) {
+        finish();
+    }
 }
